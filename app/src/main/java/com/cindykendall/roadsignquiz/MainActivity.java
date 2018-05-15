79bbb6b10e8affd2267a58743181500960cd352b
Cindy Kendall <ckck@umich.edu>
2018-05-15 12:37:48 -0400

    Added a button to view correct answers, which then display after a quiz is submitted. Upon clicking the button to take the quiz again, the answers disappear as does the view answers button. Correct the submit quiz button so it does not repeatedly add when clicked multiple times. Added one additional text question to be scored. Changed the logic on the checkbox question to be all or nothing for 3 points - the user must identify all the correct signs in order to receive points for the question, and not select the incorrect answer.

--- app/src/main/java/com/cindykendall/roadsignquiz/MainActivity.java
+++ app/src/main/java/com/cindykendall/roadsignquiz/MainActivity.java
@@ -3,6 +3,7 @@ package com.cindykendall.roadsignquiz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -12,14 +13,74 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    // set quiz score to 0
+    // set quiz score to 0 and declare variables
     int totalScore = 0;
+    RadioButton questionOneButton;
+    RadioButton questionTwoButton;
+    RadioButton questionThreeButton;
+    RadioButton questionFourButton;
+    RadioButton questionFiveButton;
+    RadioButton questionSixButton;
+    CheckBox sign1_checkbox;
+    CheckBox sign2_checkbox;
+    CheckBox sign3_checkbox;
+    CheckBox sign4_checkbox;
+    TextView scoreSummaryTextView;
+    EditText questionFormBox;
+    RadioGroup radioGroupOne;
+    RadioGroup radioGroupTwo;
+    RadioGroup radioGroupThree;
+    RadioGroup radioGroupFour;
+    RadioGroup radioGroupFive;
+    RadioGroup radioGroupSix;
+    EditText question8_answer;
+    String resetMessage;
+    Button viewAnswers;
+    TextView sign1_answer;
+    TextView sign2_answer;
+    TextView sign3_answer;
+    TextView sign4_answer;
+    TextView sign5_answer;
+    TextView sign6_answer;
+    TextView sign7_answer;
+    TextView sign8_answer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        // initialize variables
+        questionOneButton = findViewById(R.id.stop_radio_button);
+        questionTwoButton  = findViewById(R.id.bump_radio_button);
+        questionThreeButton = findViewById(R.id.pass_radio_button);
+        questionFourButton = findViewById(R.id.lowBeam_radio_button);
+        questionFiveButton = findViewById(R.id.rocks_radio_button);
+        questionSixButton = findViewById(R.id.railroad_radio_button);
+        sign1_checkbox = findViewById(R.id.sign1_checkbox);
+        sign2_checkbox = findViewById(R.id.sign2_checkbox);
+        sign3_checkbox = findViewById(R.id.sign3_checkbox);
+        sign4_checkbox =  findViewById(R.id.sign4_checkbox);
+        scoreSummaryTextView = findViewById(R.id.total_score_text_view);
+        questionFormBox = findViewById(R.id.name);
+        radioGroupOne = findViewById(R.id.question_one_radio_group);
+        radioGroupTwo = findViewById(R.id.question_two_radio_group);
+        radioGroupThree = findViewById(R.id.question_three_radio_group);
+        radioGroupFour = findViewById(R.id.question_four_radio_group);
+        radioGroupFive = findViewById(R.id.question_five_radio_group);
+        radioGroupSix = findViewById(R.id.question_six_radio_group);
+        resetMessage = resetMessage();
+        question8_answer = findViewById(R.id.question8_answer);
+        viewAnswers = findViewById(R.id.view_answers);
+        sign1_answer = findViewById(R.id.sign1_answer_display);
+        sign2_answer = findViewById(R.id.sign2_answer_display);
+        sign3_answer = findViewById(R.id.sign3_answer_display);
+        sign4_answer = findViewById(R.id.sign4_answer_display);
+        sign5_answer = findViewById(R.id.sign5_answer_display);
+        sign6_answer = findViewById(R.id.sign6_answer_display);
+        sign7_answer = findViewById(R.id.sign7_answer_display);
+        sign8_answer = findViewById(R.id.sign8_answer_display);
+        }
 
 
     // submit the quiz for scoring
@@ -30,40 +91,17 @@ public class MainActivity extends AppCompatActivity {
         nameField = findViewById(R.id.name);
         String name = nameField.getText().toString();
 
-
         //identify correct answers
-        RadioButton questionOneButton;
-        questionOneButton = findViewById(R.id.stop_radio_button);
         boolean stop_radio_buttonisChecked = questionOneButton.isChecked();
-
-        RadioButton questionTwoButton;
-        questionTwoButton = findViewById(R.id.bump_radio_button);
         boolean bump_radio_buttonisChecked = questionTwoButton.isChecked();
-
-        RadioButton questionThreeButton = findViewById(R.id.pass_radio_button);
-        boolean pass_radio_buttonisChecked;
-        pass_radio_buttonisChecked = questionThreeButton.isChecked();
-
-        RadioButton questionFourButton;
-        questionFourButton = findViewById(R.id.lowBeam_radio_button);
+        boolean pass_radio_buttonisChecked = questionThreeButton.isChecked();
         boolean lowBeam_radio_buttonisChecked = questionFourButton.isChecked();
-
-        RadioButton questionFiveButton;
-        questionFiveButton = findViewById(R.id.rocks_radio_button);
         boolean rocks_radio_buttonisChecked = questionFiveButton.isChecked();
-
-        RadioButton questionSixButton;
-        questionSixButton = findViewById(R.id.railroad_radio_button);
         boolean railroad_radio_buttonisChecked = questionSixButton.isChecked();
-
-        CheckBox sign1_checkbox = findViewById(R.id.sign1_checkbox);
         boolean sign1_checkboxisChecked = sign1_checkbox.isChecked();
-
-        CheckBox sign2_checkbox =  findViewById(R.id.sign2_checkbox);
         boolean sign2_checkboxisChecked = sign2_checkbox.isChecked();
-
-        CheckBox sign3_checkbox = findViewById(R.id.sign3_checkbox);
         boolean sign3_checkboxisChecked = sign3_checkbox.isChecked();
+        boolean sign4_checkboxisChecked = sign4_checkbox.isChecked();
 
         double totalScore = calculateScore(
                 stop_radio_buttonisChecked,
@@ -74,23 +112,34 @@ public class MainActivity extends AppCompatActivity {
                 railroad_radio_buttonisChecked,
                 sign1_checkboxisChecked,
                 sign2_checkboxisChecked,
-                sign3_checkboxisChecked
+                sign3_checkboxisChecked,
+                sign4_checkboxisChecked
         );
+
+            EditText editText = question8_answer;
+            String question8_answer = editText.getText().toString().toUpperCase();
+            if (question8_answer.equals("LLAMA"))
+                totalScore = totalScore+1;
 
         // Display the score on the screen
         String scoreMessage = createScoreMessage(name, totalScore);
         displayMessage(scoreMessage);
+
+        //Show the View Answers Button
+        viewAnswers.setVisibility(View.VISIBLE);
+
 
     }
     // create the score message on screen and one of two toast messages based on score result
     private String createScoreMessage(String name, double totalScore) {
         String scoreMessage = name;
         scoreMessage = scoreMessage + getString(R.string.result_part1) + totalScore + getString(R.string.result_part2);
+        String scoreMessagePos = scoreMessage + getString(R.string.positive_message);
+        String scoreMessageStudy = scoreMessage + getString(R.string.study_message);
         if (totalScore>7) {
-            Toast.makeText(this, R.string.positive_message, Toast.LENGTH_LONG).show();
-        }
-        else
-            Toast.makeText(this, R.string.study_message, Toast.LENGTH_LONG).show();
+            Toast.makeText(this, scoreMessagePos, Toast.LENGTH_LONG).show();
+        } else
+            Toast.makeText(this, scoreMessageStudy, Toast.LENGTH_LONG).show();
         return scoreMessage;
     }
 
@@ -100,11 +149,12 @@ public class MainActivity extends AppCompatActivity {
     }
     // This message displays when the user clicks the take quiz again (reset) button
     private String resetMessage() {
-        String scoreMessage = "Total Score: " + totalScore;
-        return scoreMessage;
+        return "Total Score: " + totalScore;
     }
 
+
     // Calculate the score
+
     private double calculateScore(
             boolean stop_radio_buttonisChecked,
             boolean bump_radio_buttonisChecked,
@@ -114,9 +164,10 @@ public class MainActivity extends AppCompatActivity {
             boolean railroad_radio_buttonisChecked,
             boolean sign1_checkboxisChecked,
             boolean sign2_checkboxisChecked,
-            boolean sign3_checkboxisChecked)
-
+            boolean sign3_checkboxisChecked,
+            boolean sign4_checkboxisChecked)
     {
+        totalScore = 0;
 
         if (stop_radio_buttonisChecked) {
             totalScore=totalScore+1;
@@ -136,14 +187,11 @@ public class MainActivity extends AppCompatActivity {
         if (railroad_radio_buttonisChecked) {
             totalScore=totalScore+1;
         }
-        if (sign1_checkboxisChecked) {
-            totalScore=totalScore+1;
-        }
-        if (sign2_checkboxisChecked) {
-            totalScore=totalScore+1;
-        }
-        if (sign3_checkboxisChecked) {
-            totalScore=totalScore+1;
+        if (sign1_checkboxisChecked &&
+                sign2_checkboxisChecked &&
+                sign3_checkboxisChecked &&
+                !sign4_checkboxisChecked) {
+            totalScore=totalScore+3;
         }
         return totalScore;
     }
@@ -153,43 +201,44 @@ public class MainActivity extends AppCompatActivity {
     public void resetButton(View view) {
 
         totalScore=0;
-
-        EditText questionFormBox = findViewById(R.id.name);
         questionFormBox.setText("");
-
-        RadioGroup radioGroupOne = findViewById(R.id.question_one_radio_group);
         radioGroupOne.clearCheck();
-
-        RadioGroup radioGroupTwo = findViewById(R.id.question_two_radio_group);
         radioGroupTwo.clearCheck();
-
-        RadioGroup radioGroupThree = findViewById(R.id.question_three_radio_group);
         radioGroupThree.clearCheck();
-
-        RadioGroup radioGroupFour = findViewById(R.id.question_four_radio_group);
         radioGroupFour.clearCheck();
-
-        RadioGroup radioGroupFive = findViewById(R.id.question_five_radio_group);
         radioGroupFive.clearCheck();
-
-        RadioGroup radioGroupSix = findViewById(R.id.question_six_radio_group);
         radioGroupSix.clearCheck();
-
-        CheckBox sign1_checkbox =  findViewById(R.id.sign1_checkbox);
         sign1_checkbox.setChecked(false);
-
-        CheckBox sign2_checkbox =  findViewById(R.id.sign2_checkbox);
         sign2_checkbox.setChecked(false);
-
-        CheckBox sign3_checkbox =  findViewById(R.id.sign3_checkbox);
         sign3_checkbox.setChecked(false);
-
-        CheckBox sign4_checkbox =  findViewById(R.id.sign4_checkbox);
         sign4_checkbox.setChecked(false);
-
+        question8_answer.setText("");
         String resetMessage = resetMessage();
         displayMessage(resetMessage);
 
-    }
+        //Make answers invisible and make submit visible
+         viewAnswers.setVisibility(View.INVISIBLE);
 
+         //To hide all the answers made visible
+          sign1_answer.setVisibility(View.INVISIBLE);
+          sign2_answer.setVisibility(View.INVISIBLE);
+          sign3_answer.setVisibility(View.INVISIBLE);
+          sign4_answer.setVisibility(View.INVISIBLE);
+          sign5_answer.setVisibility(View.INVISIBLE);
+          sign6_answer.setVisibility(View.INVISIBLE);
+          sign7_answer.setVisibility(View.INVISIBLE);
+          sign8_answer.setVisibility(View.INVISIBLE);
+        }
+
+        //Show answer text boxes
+        public void viewAnswers(View view) {
+        sign1_answer.setVisibility(View.VISIBLE);
+        sign2_answer.setVisibility(View.VISIBLE);
+        sign3_answer.setVisibility(View.VISIBLE);
+        sign4_answer.setVisibility(View.VISIBLE);
+        sign5_answer.setVisibility(View.VISIBLE);
+        sign6_answer.setVisibility(View.VISIBLE);
+        sign7_answer.setVisibility(View.VISIBLE);
+        sign8_answer.setVisibility(View.VISIBLE);
+    }
 }
